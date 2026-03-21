ca4e430620b22433900329ca4225eb557fb38981
Andreas Weder <andreas@weder.net>
2026-03-21 14:00:29 +0100

    Removed no longer used LastschriftParser - it has been replaced by dedicated browsers.

--- src/notification/parsers/__init__.py
+++ src/notification/parsers/__init__.py
@@ -3,7 +3,6 @@
 from notification.parsers.apple_pay_parser import ApplePayParser
 from notification.parsers.dauerauftrag_parser import DauerauftragParser
 from notification.parsers.debit_direct_parser import DebitDirectParser
-from notification.parsers.lastschrift_parser import LastschriftParser
 from notification.parsers.twint_senden_parser import TwintSendenParser
 from notification.parsers.zahlung_parser import ZahlungParser
 
@@ -13,5 +12,4 @@ __all__ = [
     "DebitDirectParser",
     "DauerauftragParser",
     "ZahlungParser",
-    "LastschriftParser",
 ]
